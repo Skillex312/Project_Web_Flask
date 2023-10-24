3b17c860a20e7a61bcd725d1bf339693e9067f1a
Sebastian Ramirez <106999399+Skillex312@users.noreply.github.com>
2023-10-24 09:15:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,39 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, jsonify
 from Conexion_bd import ConexionBD
+
+
 app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template('index.html')
 
-@app.route('/signin')
-def showSignin():
-    return render_template('sign_in.html')
-
-"""def validar_login('username, password'):
-    conexion = ConexionBD('localhost', 'root', 'DESCONocido312', 'academia')
-    consulta = ("SELECT * FROM Usuarios WHERE ID = %s AND Password = %s")
-    parametros = (username, password)
-    resultado = conexion.ejecutar_consulta(consulta, parametros)
-    if resultado:
-        print("Usuario encontrado")
-        return redirect('/success')
-    else:
-        print("Usuario no encontrado")
-        return redirect('/error')"""
+@app.route('/sign')
+def showSign():
+    return render_template('sign.html')
 
 @app.route('/validar_login', methods=['POST'])
-def validar_login():
-    # Obtener los valores de los campos de entrada
-    nombre = request.json['nombre']
-    contrasena = request.json['contrasena']
+def valida_login():
+    # Obtener los valores de los campos de entrada desde la solicitud POST
+    username = request.form['username']
+    password = request.form['password']
+
+    print('username:', username)
+    print('password:', password)
+    # Conectar a la base de datos
+    conexion = ConexionBD('localhost', 'root', 'M4f3s1t43312.', 'academia')
+
+    # Ejecutar una consulta para obtener el usuario con el nombre de usuario y la contraseña proporcionados
+    consulta = "SELECT * FROM Usuarios WHERE ID = %s AND Password = %s"
+    parametros = (username, password)
+    resultado = conexion.ejecutar_consulta(consulta, parametros) 
 
     # Validar las credenciales
-    if nombre == 'ID' and contrasena == 'Contrasena':
-        resultado = True
+    if resultado:
+        print("Usuario encontrado")
+        return jsonify({'valid': True})
     else:
-        resultado = False
+        print("Usuario no encontrado")
+        return jsonify({'valid': False})
 
-    # Devolver el resultado de la validación como un objeto JSON
-    return {'resultado': resultado}
-
-@app.route('/home_login_exitoso')
-def home_login_exitoso():
-    return render_template('home_login.html')
-
-@app.route('/home_login_error')
-def home_login_error():
-    return render_template('error.html')
-    
 if __name__ == "__main__":
     app.run()
