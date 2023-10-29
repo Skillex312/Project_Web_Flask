324c0faaafdde9bc5dd62870e189cebd0e828463
Sebastian Ramirez <106999399+Skillex312@users.noreply.github.com>
2023-10-29 11:11:27 -0500

    Update Conexion_bd.py

--- Conexion_bd.py
+++ Conexion_bd.py
@@ -1,50 +1,40 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="M4f3s1t43312.",
-  port=3306,
-  database="academia"
-)
-
-"""
-# Get data
-
-mysql = "select * from usuarios"
-mycursor = mydb.cursor()
-mycursor.execute(mysql)
-myresult = mycursor.fetchall()
-
-# Do something
-
-df = pd.DataFrame()
-for x in myresult:
-  df2 = pd.DataFrame(list[x]).T
-  df = pd.concat([df, df2])
-  
-df.to_html('/home_login')
-"""
-
-
-
-
-# Establecer la conexión con la base de datos
 class ConexionBD:
-  def __init__(self, host, user, password, database):
-    try:
-      print("Conectando a la base de datos...")
-      self.connection = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-      self.cursor = self.connection.cursor()
-    except mysql.connector.Error as error:
-      print("Error al conectarse a la base de datos: {}".format(error))
-      raise
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = None
+        self.cursor = None
 
-  def ejecutar_consulta(self, consulta, parametros = None):
-    self.cursor.execute(consulta, parametros)
-    return self.cursor.fetchall()
+    def conectar(self, host, user, password):
+        try:
+            self.connection = mysql.connector.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=self.database
+            )
+            if self.connection.is_connected():
+                print("Conexión exitosa")
+            else:
+                print("Conexión fallida")
+            self.cursor = self.connection.cursor()
+        except mysql.connector.Error as error:
+            print("Error al conectarse a la base de datos: {}".format(error))
+            raise
+
+    def ejecutar_consulta(self, consulta, parametros=None):
+        if not self.cursor:
+            self.conectar()  # Conecta si el cursor no está inicializado
+
+        self.cursor.execute(consulta, parametros)
+        return self.cursor.fetchall()
+
+    def cerrar_conexion(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.connection:
+            self.connection.close()
